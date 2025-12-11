aa68ce913382dc5d6fc9bd715aaf7b0795808521
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-11 17:54:34 +0200

    Create client.py

---
+++ client.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+
+# Instantiate a new discord client
+client: commands.Bot = commands.Bot(command_prefix='!',
+                                    case_insensitive=True,
+                                    help_command=None,
+                                    intents=discord.Intents.all()
+                                    )

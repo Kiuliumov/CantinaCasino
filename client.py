346e95b7e798d046d178827aa00b83b11402ef58
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-11 18:13:25 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,8 +3,11 @@ from discord.ext import commands
 
 
 # Instantiate a new discord client
-client: commands.Bot = commands.Bot(command_prefix='!',
-                                    case_insensitive=True,
-                                    help_command=None,
-                                    intents=discord.Intents.all()
-                                    )
+client: commands.Bot = commands.Bot(
+
+    command_prefix='!',
+    case_insensitive=True,
+    help_command=None,
+    intents=discord.Intents.all()
+
+)

38b29d3346dc7dfb828e3464a4579fe2333f1c1c
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-14 09:05:26 +0200

    Db

--- app.py
+++ app.py
@@ -1,47 +1,38 @@
 import os
 from client import client
 import dotenv
+import discord
 
 dotenv.load_dotenv()
-
 token: str = os.getenv('TOKEN')
-
-loaded_commands = []
 
 @client.event
 async def setup_hook():
-    global loaded_commands
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             cog_name = f"cogs.{filename[:-3]}"
             await client.load_extension(cog_name)
             print(f"Loaded extension: {filename}")
-            cog = client.get_cog(filename[:-3])
-            if cog:
-                for command in cog.get_commands():
-                    loaded_commands.append(command.name)
 
 @client.event
 async def on_ready():
+    try:
+        GUILD_ID = 776155094868819999
+        guild = discord.Object(id=GUILD_ID)
+        synced_commands = await client.tree.sync(guild=guild)
+        synced_names = [cmd.name for cmd in synced_commands]
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
+        synced_names = []
+
     print("==============================")
     print("🎰 Cantina Casino Bot is Online! 🎰")
     print(f"Application name: {client.user.name}")
     print(f"Application ID   : {client.user.id}")
-    print(f"Servers  : {len(client.guilds)}")
-    if loaded_commands:
-        print(f"Loaded Commands: {', '.join(loaded_commands)}")
+    print(f"Servers          : {len(client.guilds)}")
+    print(f"Users: {len(client.users)}")
+    if synced_names:
+        print(f"Synced Commands  : {', '.join(synced_names)}")
     print("==============================")
-
-    @client.event
-    async def on_ready():
-        await client.tree.sync()
-        print("==============================")
-        print("🎰 Cantina Casino Bot is Online! 🎰")
-        print(f"Application name: {client.user.name}")
-        print(f"Application ID   : {client.user.id}")
-        print(f"Servers  : {len(client.guilds)}")
-        if loaded_commands:
-            print(f"Loaded Commands: {', '.join(loaded_commands)}")
-
 
 client.run(token)
